67b3f74f3e34ee9aaa6f8adf3d77d36743a4cd47
miszc <miszczykmiloszk@gmail.com>
2025-11-26 15:22:50 +0100

    Add

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+
+const dataStartowa = new Date('2024-11-25T19:00:00');
+
+function App() {
+    const [roznica, setRoznica] = useState(new Date() - dataStartowa);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const teraz = new Date();
+            setRoznica(teraz - dataStartowa);
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const sekundy = Math.floor((roznica / 1000) % 60);
+    const minuty = Math.floor((roznica / (1000 * 60)) % 60);
+    const godziny = Math.floor((roznica / (1000 * 60 * 60)) % 24);
+    const dni = Math.floor(roznica / (1000 * 60 * 60 * 24));
+
+    const formatTime = (time) => String(time).padStart(2, '0');
+
+    return (
+        <>
+            <h1>Razem od</h1>
+            <div className="timer">
+                <div>
+                    <span>{dni}</span>
+                    <span>DNI</span>
+                </div>
+                <div>
+                    <span>{formatTime(godziny)}</span>
+                    <span>GODZ</span>
+                </div>
+                <div>
+                    <span>{formatTime(minuty)}</span>
+                    <span>MIN</span>
+                </div>
+                <div>
+                    <span>{formatTime(sekundy)}</span>
+                    <span>SEK</span>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
